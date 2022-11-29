1eeee952602d18fb8e35ad7b6beebae01509af33
YM162 <david.fontaneda@estudiante.uam.es>
2022-11-29 17:35:52 +0100

    Dimensions and initial cells are now defined via a scenario struct. This struct can be created with the create_scenario() multiple-dispatched function for all dimensions supported.

--- functions.jl
+++ functions.jl
@@ -35,7 +35,13 @@ end
 end
 
 #We initialize the model according to some parameters.
-function  model_init(;pr,dr,mr,seed,x,y,z,ngenes,fitness,cell_pos,wall_pos,treatment)
+function  model_init(;seed,pr,dr,mr,ngenes,fitness,scenario,treatment)
+    x = scenario.x
+    y = scenario.y
+    z = scenario.z
+    cell_pos = scenario.cell_pos
+    wall_pos = scenario.wall_pos
+    
     
     fitness = Dict(zip([BitArray(i) for i in keys(fitness)],[fitness[i] for i in keys(fitness)]))
     fitness=DefaultDict(0,fitness)
@@ -216,6 +222,12 @@ function get_scenario(filepath)
     return dims[2],dims[1],cell_pos,wall_pos #We return height, width, the positions of the cells and the positions of the walls
 end
 
+#Function to go from BitArray to Int. Taken from https://discourse.julialang.org/t/parse-an-array-of-bits-bitarray-to-an-integer/42361/5
+function bit_2_int(arr)
+    arr = reverse(arr)
+    sum(((i, x),) -> Int(x) << ((i-1) * sizeof(x)), enumerate(arr.chunks))
+end
+
 #We define what a treatment is
 mutable struct Treatment
     pausing_size::Int 
@@ -225,8 +237,67 @@ mutable struct Treatment
     active::Bool
 end
 
-#Function to go from BitArray to Int. Taken from https://discourse.julialang.org/t/parse-an-array-of-bits-bitarray-to-an-integer/42361/5
-function bit_2_int(arr)
-    arr = reverse(arr)
-    sum(((i, x),) -> Int(x) << ((i-1) * sizeof(x)), enumerate(arr.chunks))
+#We define the scenario and the functions to create it using multiple dispatch.
+struct Scenario
+    x::Int
+    y::Int
+    z::Int
+    cell_pos::Vector
+    wall_pos::Vector
+end
+
+#0D
+function create_scenario(size::Int64,ncells::Int)   
+    return Scenario(size,0,0,[(1,1,1) for i in 1:ncells],[])
+end
+
+#1D
+function create_scenario(size::Tuple{Int64},ncells::Int,cell_pos::String="center",wall_pos::Vector=[])
+    if cell_pos=="random"
+        cell_pos = [((rand(1:size[1])),1,1) for i in 1:ncells]
+    elseif cell_pos=="center"
+        cell_pos = [(Int(floor(size[1]/2))+1,1,1) for i in 1:ncells]
+    else
+        print("Error, cell_pos option not found.")
+        return
+    end
+        return Scenario(size[1],1,1,cell_pos,wall_pos)
+end
+
+function create_scenario(size::Tuple{Int64},cell_pos::Vector{Tuple{Int64}},wall_pos=[])
+        return Scenario(size[1],1,1,[(x[1],1,1) for x in cell_pos],wall_pos)
+end
+
+#2D
+function create_scenario(size::Tuple{Int64, Int64},ncells::Int,cell_pos::String="center",wall_pos::Vector=[])
+    if cell_pos=="random"
+        cell_pos = [((rand(1:size[1])),(rand(1:size[2])),1) for i in 1:ncells]
+    elseif cell_pos=="center"
+        cell_pos = [(Int(floor(size[1]/2))+1,Int(floor(size[2]/2))+1,1) for i in 1:ncells]
+    else
+        print("Error, cell_pos option not found.")
+        return
+    end
+        return Scenario(size[1],size[2],1,cell_pos,wall_pos)
+end
+
+function create_scenario(size::Tuple{Int64,Int64},cell_pos::Vector{Tuple{Int64, Int64}},wall_pos::Vector=[])
+        return Scenario(size[1],size[2],1,[(x[1],x[2],1) for x in cell_pos],wall_pos)
+end
+
+#3D
+function create_scenario(size::Tuple{Int64, Int64, Int64},ncells::Int,cell_pos::String="center",wall_pos::Vector=[])
+    if cell_pos=="random"
+        cell_pos = [((rand(1:size[1])),(rand(1:size[2])),(rand(1:size[3]))) for i in 1:ncells]
+    elseif cell_pos=="center"
+        cell_pos = [(Int(floor(size[1]/2))+1,Int(floor(size[2]/2))+1,Int(floor(size[3]/2))+1) for i in 1:ncells]
+    else
+        print("Error, cell_pos option not found.")
+        return
+    end
+        return Scenario(size[1],size[2],size[3],cell_pos,wall_pos)
+end
+
+function create_scenario(size::Tuple{Int64,Int64,Int64},cell_pos::Vector{Tuple{Int64, Int64,Int64}},wall_pos::Vector=[])
+        return Scenario(size[1],size[2],size[3],[(x[1],x[2],x[3]) for x in cell_pos],wall_pos)
 end
