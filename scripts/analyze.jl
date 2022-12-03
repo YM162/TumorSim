b2764af37484d284a0cd5b184f7e579a5b4af885
YM162 <david.fontaneda@estudiante.uam.es>
2022-12-03 02:10:46 +0100

    changed the savename of the simulations.

--- scripts/analyze.jl
+++ scripts/analyze.jl
@@ -8,12 +8,14 @@ using Statistics
 using HypothesisTests
 
 df = collect_results(datadir("simulations"))
-print(df)
+println(df)
+
 println("Adaptive therapy:")
 adaptive_TTP = filter(n -> n !=-1,filter("t_pausing_size" => n -> n == 1000, df)[!,"TTP"])
-println("Mean -",mean(adaptive_TTP))
+println("Mean ",mean(adaptive_TTP))
 println("Continuous therapy:")
 continuous_TTP = filter(n -> n !=-1,filter("t_pausing_size" => n -> n == 0, df)[!,"TTP"])
-println("Mean -",mean(continuous_TTP))
+println("Mean ",mean(continuous_TTP))
 
+println("t-test p-value:")
 pvalue(EqualVarianceTTest([x for x in adaptive_TTP], [x for x in continuous_TTP]))
--- scripts/run_batch.jl
+++ scripts/run_batch.jl
@@ -6,29 +6,53 @@ using Distributed
 @everywhere using TumorSim
 
 using ProgressMeter
-#We create the fitness, scenarios and therapies we want to use
-fitness=Dict([0,0,0]=>1, 
+#With the same fitness we can change the cost of resistance
+fitness1=Dict([0,0,0]=>1, 
+                [1,0,0]=>1.3,
+                [0,1,0]=>1.2,
+                [1,1,0]=>1.5,
+                [1,1,1]=>0.6)
+
+fitness2=Dict([0,0,0]=>1, 
+                [1,0,0]=>1.3,
+                [0,1,0]=>1.2,
+                [1,1,0]=>1.5,
+                [1,1,1]=>0.8)
+
+fitness3=Dict([0,0,0]=>1, 
+                [1,0,0]=>1.3,
+                [0,1,0]=>1.2,
+                [1,1,0]=>1.5,
+                [1,1,1]=>1)
+
+fitness4=Dict([0,0,0]=>1, 
                 [1,0,0]=>1.3,
                 [0,1,0]=>1.2,
                 [1,1,0]=>1.5,
                 [1,1,1]=>1.2)
 
+fitness5=Dict([0,0,0]=>1, 
+                [1,0,0]=>1.3,
+                [0,1,0]=>1.2,
+                [1,1,0]=>1.5,
+                [1,1,1]=>1.5)
+#We test the dimensionality.
 scenario_0D = create_scenario((1000000),10)
 scenario_1D = create_scenario((1000000,),10,"center")
 scenario_2D = create_scenario((1000,1000),10,"center")
 scenario_3D = create_scenario((100,100,100),10,"center")
 
+#We test adaptive and continuous therapy
 adaptive_therapy = create_treatment(3000, 2000, 1000, 3, 0.75) 
 continuous_therapy = create_treatment(3000, 2000, 0, 3, 0.75) 
 
-#This would be cool to do, but we need the cluster, because its 1.000.000 simulations for each fitness landscape. Would take 2 months on my computer.
-
+#This would be cool to do, but we need the cluster, because its 5.000.000 simulations for each fitness landscape. Would take 10 months on my computer.
 parameters = Dict(
-    "pr" => [0.005,0.01,0.0015,0.02,0.025],
+    "pr" => [0.01,0.02,0.03,0,04,0.05],
     "dr" => [0,0.2,0.4,0.6,0.8],
     "mr" => [0.001,0.005,0.01,0.05,0.1],   
     "scenario" => [scenario_0D,scenario_1D,scenario_2D,scenario_3D], 
-    "fitness" => fitness,
+    "fitness" => [fitness1,fitness2,fitness3,fitness4,fitness5],
     "treatment" => [adaptive_therapy,continuous_therapy],
     "seed" => map(abs,rand(Int64,1000))
 )
@@ -39,9 +63,9 @@ parameters = Dict(
     "dr" => 0.55,
     "mr" => 0.01,   
     "scenario" => scenario_3D, 
-    "fitness" => fitness,
+    "fitness" => fitness4,
     "treatment" => [adaptive_therapy,continuous_therapy],
-    "seed" => map(abs,rand(Int64,100))
+    "seed" => map(abs,rand(Int64,5))
 )
 
 parameter_combinations = dict_list(parameters)
@@ -51,5 +75,5 @@ steps=3000
 results = @showprogress pmap(simulate,parameter_combinations,fill(steps,length(parameter_combinations)))
 
 for (i, d) in enumerate(parameter_combinations)
-    safesave(datadir("simulations", savename(d, "jld2")), results[i])
+    safesave(datadir("simulations", savename(results[i], "jld2")), results[i])
 end
