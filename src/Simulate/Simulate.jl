7066b7f9ce449f7b48b6e8122cc87d28fb75dbb8
YM162 <david.fontaneda@estudiante.uam.es>
2022-12-04 03:07:10 +0100

    Removed Interactive simulations for the moment.

--- src/Simulate/Simulate.jl
+++ src/Simulate/Simulate.jl
@@ -1,32 +1,6 @@
 
 using StatsBase
 
-function launch_interactive_simulation(d::Dict) #Broken ATM until i figure out the GLMakie problems.
-    @unpack seed, pr, dr, mr, fitness, scenario, treatment = d
-
-    model = model_init(pr=0.027, dr=0.015, mr=0.01, scenario=scenario, fitness=fitness,treatment=treatment, seed=0)
-
-    #We plot the genotype of each cell with a different color.
-    genotypes = [replace(string(x)," "=>"") for x in sort!([x for x in keys(fitness)],by=x -> bit_2_int(BitArray(x)))]
-    genotype_bits = [x for x in sort!([x for x in keys(fitness)],by=x -> bit_2_int(BitArray(x)))]
-    order = Dict(zip(genotype_bits,1:length(genotype_bits)))
-
-    #Functions to get a different color for each genotype
-    genotypecolor(a) = get(colorschemes[:hsv], order[a.genotype], (1,length(genotypes)+1))
-    genotypecolor_legend(a) = get(colorschemes[:hsv], a, (1,length(genotypes)+1))
-
-    #We make a dynamic plot
-    figure, _ = abmplot(model;agent_step! = agent_step!,model_step! = model_step!,ac = genotypecolor,as=0.5)
-
-    #We create a legend for the genotypes
-    Legend(figure[1, 2],
-        [MarkerElement(color = genotypecolor_legend(a), marker = '■', markersize = 15, strokecolor = :black) for a in 1:length(genotypes)],
-        genotypes, patchsize = (20, 20), rowgap = 1)
-
-    #We display the figure
-    display(figure)
-end
-
 function simulate(d::Dict,steps::Int)
     @unpack seed, pr, dr, mr, fitness, scenario, treatment = d
     
