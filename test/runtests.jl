007133f1df025a383de19c0ccfd24567868a22c3
YM162 <david.fontaneda@estudiante.uam.es>
2023-05-24 22:47:53 +0200

    Added more tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,11 +22,16 @@ ti = time()
     @test TumorSim.bit_2_int(BitArray([0,0,0,0,0])) == 0
     @test TumorSim.bit_2_int(BitArray([1,1,1])) == 7
     #Generate fitness from restrictions
-    restrictions= [[1,2,1],[1,3,1],[2,4,1],[3,4,1]]
-    mitosis_probabilities = TumorSim.Fitness.build_fitness_table(restrictions,0.027,[1.16,1.35],0.15,3)
+    restrictions_AND= [[1,2,1],[1,3,1],[2,4,1],[3,4,1]]
+    mitosis_probabilities = TumorSim.Fitness.build_fitness_table(restrictions_AND,0.027,[1.16,1.35],0.15,3)
     @test round(mitosis_probabilities[[0,0,0]],digits=10)==round(0.027,digits=10)
     @test round(mitosis_probabilities[[1,0,0]],digits=10)==round(0.027*1.16,digits=10)
     @test round(mitosis_probabilities[[1,1,1]],digits=10)==round(0.027*1.16*1.35*(1-0.15),digits=10)
+    restrictions_OR= [[1,2,1],[1,3,1],[2,4,2],[3,4,2]]
+    mitosis_probabilities = TumorSim.Fitness.build_fitness_table(restrictions_OR,0.027,[1.16,1.35],0.15,3)
+    @test round(mitosis_probabilities[[0,0,0]],digits=10)==round(0.027,digits=10)
+    @test round(mitosis_probabilities[[1,0,0]],digits=10)==round(0.027*1.16,digits=10)
+    @test round(mitosis_probabilities[[1,0,1]],digits=10)==round(0.027*1.16*(1-0.15),digits=10)
 end
 
 @testset "Scenario tests" begin
@@ -69,6 +74,12 @@ end
     @test scenario.y == 17
     @test scenario.z == 18
     @test length(scenario.cell_pos) == 3
+    #Errors
+    @test create_scenario((10,10),1000) === nothing
+    @test create_scenario((10,),1000,"test") === nothing
+    @test create_scenario((10,10),1000,"test") === nothing
+    @test create_scenario((5,5,5),1000,"test") === nothing
+
 end
 
 @testset "Treatment tests" begin
@@ -103,6 +114,7 @@ end
 #Prepare a simulation
 treatment = create_treatment(3000,0.65,0.5,3,0.75)
 scenario = create_scenario((100,100,100),5,"center")
+scenario2 = create_scenario((100,100,100),5,"center",true)
 fitness=Dict([0,0,0]=>0.027, 
             [1,0,0]=>0.035,
             [0,1,0]=>0.032,
@@ -110,10 +122,10 @@ fitness=Dict([0,0,0]=>0.027,
             [1,1,1]=>0.032)
 
 params = Dict(
-    "interaction_rule"=>:contact,
     "death_rate" => [0.55,2],
+    "interaction_rule"=>[:contact,:hierarchical_voter],
     "mutation_rate" => 0.1,
-    "scenario" => scenario, 
+    "scenario" => [scenario,scenario2], 
     "fitness" => fitness,
     "treatment" => treatment,
     "migration_rate" => 0.05,
@@ -121,14 +133,17 @@ params = Dict(
 )
 
 dicts = dict_list(params)
+steps=100
+results_short = pmap(simulate,dicts,fill(steps,length(dicts)))
 steps=3000
 results = pmap(simulate,dicts,fill(steps,length(dicts)))
 adata1 = results[1]["Genotypes"]
 adata2 = results[2]["Genotypes"]
+adata_short = results_short[1]["Genotypes"]
 resistant_inhibited1 = results[1]["Resistant_inhibited_by"]
 
 @testset "Simulate tests" begin
-    @test length(results) == 2
+    @test length(results) == 8
     @test length(results[1]) == 24
     @test length(eachcol(adata1)) == 6
     @test length(eachrow(adata1)) != 0
@@ -138,6 +153,8 @@ end
 @testset "Analysis tests" begin
     TTP1 = get_TTP(adata1,3200)
     TTP2 = get_TTP(adata2,3200)
+    TTP_short = get_TTP(adata_short,100)
+
     @test TTP1 != -1
     @test TTP2 == -2
 
