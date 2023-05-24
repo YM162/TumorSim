02642fe3060221f65ba15d159299c9cc77d5881c
YM162 <david.fontaneda@estudiante.uam.es>
2023-05-24 21:28:47 +0200

    Added new tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,12 @@ ti = time()
     @test TumorSim.bit_2_int(BitArray([1,0,0,0,0])) == 16
     @test TumorSim.bit_2_int(BitArray([0,0,0,0,0])) == 0
     @test TumorSim.bit_2_int(BitArray([1,1,1])) == 7
+    #Generate fitness from restrictions
+    restrictions= [[1,2,1],[1,3,1],[2,4,1],[3,4,1]]
+    mitosis_probabilities = TumorSim.Fitness.build_fitness_table(restrictions,0.027,[1.16,1.35],0.15,3)
+    @test round(mitosis_probabilities[[0,0,0]],digits=10)==round(0.027,digits=10)
+    @test round(mitosis_probabilities[[1,0,0]],digits=10)==round(0.027*1.16,digits=10)
+    @test round(mitosis_probabilities[[1,1,1]],digits=10)==round(0.027*1.16*1.35*(1-0.15),digits=10)
 end
 
 @testset "Scenario tests" begin
@@ -39,7 +45,7 @@ end
     @test length(scenario.cell_pos) == 1
     #2D
     scenario = create_scenario((15,20),5,"random")
-    scenario = create_scenario((15,20),5,"center")
+    scenario = create_scenario((15,20),5,"center",true)
     @test scenario.x == 15
     @test scenario.y == 20
     @test scenario.z == 1
@@ -52,7 +58,7 @@ end
     @test length(scenario.cell_pos) == 2
     #3D
     scenario = create_scenario((16,17,18),19,"random")
-    scenario = create_scenario((16,17,18),19,"center")
+    scenario = create_scenario((16,17,18),19,"center",false)
     @test scenario.x == 16
     @test scenario.y == 17
     @test scenario.z == 18
@@ -87,7 +93,7 @@ end
     @test agent.phylogeny == []
     #3D
     scenario = create_scenario((10,10,10),5,"center",true)
-    model = TumorSim.model_init(interaction_rule=:contact,death_rate=0.55, mutation_rate=0.01, migration_rate = 0.05, scenario=scenario, fitness=fitness,treatment=treatment, seed=0)
+    model = TumorSim.model_init(interaction_rule=:hierarchical_voter,death_rate=0.55, mutation_rate=0.01, migration_rate = 0.05, scenario=scenario, fitness=fitness,treatment=treatment, seed=0)
     agent = collect(allagents(model))[1]
     @test nagents(model) == 5
     @test agent.time_alive == 0
@@ -119,10 +125,11 @@ steps=3000
 results = pmap(simulate,dicts,fill(steps,length(dicts)))
 adata1 = results[1]["Genotypes"]
 adata2 = results[2]["Genotypes"]
+resistant_inhibited1 = results[1]["Resistant_inhibited_by"]
 
 @testset "Simulate tests" begin
     @test length(results) == 2
-    @test length(results[1]) == 22
+    @test length(results[1]) == 24
     @test length(eachcol(adata1)) == 6
     @test length(eachrow(adata1)) != 0
     @test length(eachrow(adata1)) > length(eachrow(adata2))
@@ -134,7 +141,7 @@ end
     @test TTP1 != -1
     @test TTP2 == -2
 
-    diversity = get_diversity(adata1)
+    diversity = TumorSim.Analysis.get_diversity(adata1)
     @test length(eachcol(diversity)) == 3
     @test length(eachrow(diversity)) != 0
     @test diversity[!,"species_richness"][1] == 1
@@ -146,10 +153,27 @@ end
     @test diversity[!,"evenness"][400] > 0
     @test diversity[!,"evenness"][400] < 1
 
+    #Test for resistance fraction
+    resistant_fraction1 = TumorSim.Analysis.get_resistant_fraction_on_detection(adata1,3200,3)
+    resistant_fraction2 = TumorSim.Analysis.get_resistant_fraction_on_detection(adata2,3200,3)
+    @test resistant_fraction1 != -1
+    @test resistant_fraction2 == -1
+    @test resistant_fraction1 > 0
+    @test resistant_fraction1 < 1
+    
+    #Test for divergence
+    divergence = TumorSim.Analysis.get_divergence(adata1,resistant_inhibited1)
+    @test length(eachcol(divergence)) == 4
+    @test length(eachrow(divergence)) != 0
+    @test minimum(divergence[!,"jensen_shannon"]) >= 0
+    @test maximum(divergence[!,"jensen_shannon"]) <= 1
+
+    #Test for jensen shannon divergence
+    @test TumorSim.Analysis.jensen_shannon([1.0,0.0],[0.0,1.0]) == 1.0
+    @test TumorSim.Analysis.jensen_shannon([1.0,0.0],[1.0,0.0]) == 0.0
+    @test TumorSim.Analysis.jensen_shannon([0.5,0.5],[0.5,0.5]) == 0.0
+    @test round(TumorSim.Analysis.jensen_shannon([0.5,0.5],[0.0,1.0]),digits=4) == 0.3113
 end
-
-#We need to write tests for the dashboard. Look into XVFB for this.
-
 
 ti = time() - ti
 println("\nTest took total time of:")
